807275f88b77b04d1c96bf98b17a3e977f5ff19c
neadianyanwu <neadianyanwu@gmail.com>
2023-05-30 13:37:34 -1100

    Tic-tac-toe 2

---
+++ script.js
@@ -0,0 +1,93 @@
+const boxs = document.querySelectorAll('.box');
+const statusTxt = document.querySelector('#status');
+const btnRestart = document.querySelector('#restart');
+
+let x = "X";
+let o = "O";
+
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = x;
+let player = "X";
+let running = false;
+
+function init() {
+  boxs.forEach(box => box.addEventListener('click', boxClick));
+  btnRestart.addEventListener('click', restartGame);
+  statusTxt.textContent = `${player} Your Turn`;
+  running = true;
+}
+
+function boxClick() {
+  const index = this.dataset.index;
+  if (options[index] !== "" || !running) {
+    return;
+  }
+  updateBox(this, index);
+  checkWinner();
+}
+
+function updateBox(box, index) {
+  options[index] = currentPlayer;
+  box.textContent = currentPlayer;
+}
+
+function changePlayer() {
+  player = player === 'X' ? 'O' : 'X';
+  currentPlayer = currentPlayer === x ? o : x;
+  statusTxt.textContent = `${player} Your Turn`;
+}
+
+function checkWinner() {
+  let isWon = false;
+  for (let i = 0; i < win.length; i++) {
+    const condition = win[i];
+    const box1 = options[condition[0]];
+    const box2 = options[condition[1]];
+    const box3 = options[condition[2]];
+    if (box1 === "" || box2 === "" || box3 === "") {
+      continue;
+    }
+    if (box1 === box2 && box2 === box3) {
+      isWon = true;
+      boxs[condition[0]].classList.add('win');
+      boxs[condition[1]].classList.add('win');
+      boxs[condition[2]].classList.add('win');
+      break;
+    }
+  }
+  if (isWon) {
+    statusTxt.textContent = `${player} Won..`;
+    running = false;
+  } else if (!options.includes("")) {
+    statusTxt.textContent = `Game Draw..!`;
+    running = false;
+  } else {
+    changePlayer();
+  }
+}
+
+function restartGame() {
+  options = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = x;
+  player = "X";
+  running = true;
+  statusTxt.textContent = `${player} Your Turn`;
+  boxs.forEach(box => {
+    box.textContent = "";
+    box.classList.remove('win');
+  });
+}
+
+// Call the init function to start the game
+init();
